82a7c811d9edfcc2adaf7dc8dece560bb8d27c51
Kriz <christian.munoz1@mail.udp.cl>
2020-06-30 00:21:48 -0400

    lunes 29

--- api.js
+++ api.js
@@ -3,7 +3,7 @@ var app = express();
 var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-var dbo,cat,prod;
+var dbo,cat,prod,compras,user;
 var ObjectID = mongo.ObjectID;
 
 MongoClient.connect(url, function(err, db) {
@@ -11,6 +11,8 @@ MongoClient.connect(url, function(err, db) {
 	dbo = db.db("tienda");
 	cat = dbo.collection("categorias");
 	prod = dbo.collection("productos");
+	compras = dbo.collection("compras");
+	user = dbo.collection("usuarios");
 });
 
 app.get("/categorias", function(req, res) {
@@ -44,23 +46,76 @@ app.get("/producto/:prod", function(req, res) {
 });
 
 app.get("/agregarCarro/:prod&:cant", function(req, res) {
-	res.send("Hello World!");
+	prod.findOne({_id:new ObjectID(req.params.prod)}, function(err, result) {
+		if (err) throw err;
+		product = {"id":result["_id"], "name":result["name"], "pre":result["pre"], "cant":parseInt(req.params.cant)};
+		addtoCart(product);
+	res.send(product);
+	});
 });
 
 app.get("/delCarro/:prod", function(req, res) {
-	res.send("Hello World!");
+	if(del(req.params.prod)==0) {
+		res.send("Producto eliminado :c");
+	}
+	res.send("Producto no encontrado en el carrito");
 });
 
 app.get("/compraCarro", function(req, res) {
-	res.send("Hello World!");
+	res.send(JSON.parse(localStorage.getItem("shoppingCart")));
 });
 
 app.get("/ordenesdeCompra", function(req, res) {
-	res.send("Hello World!");
+	compras.find({}).toArray( async function(err, result) {
+		if (err) throw err;
+		var arr = [];
+		var cont = 0;
+		var carrito = {};
+		var prods, comprador, response, lista, orden;
+		for (var i in result) {
+			orden = {}
+			lista = result[i]["listaVenta"].split(",")
+			cont = 0;
+			carrito = {};
+			response = await user.findOne({_id:new ObjectID(result[i]["comprador"])});
+			comprador = response["name"];
+			for (let x = 0; x < lista.length; x+=2) {
+				prods = {};
+				response = await prod.findOne({_id:new ObjectID(lista[x])});
+				prods["name"] = response["name"];
+				prods["pre"] = response["pre"];
+				carrito[cont] = prods;
+				cont++;
+			}
+			orden[comprador] = carrito;
+			arr.push(orden);
+		}
+		res.send(arr);
+	});
 });
 
 app.get("/orden/:id", function(req, res) {
-	res.send("Hello World!");
+	compras.findOne({_id:new ObjectID(req.params.id)}, async function(err, result) {
+		if (err) throw err;
+		var cont = 0;
+		var carrito = {};
+		var prods, comprador, response;
+		var orden = {}
+		var lista = result["listaVenta"].split(",")
+		response = await user.findOne({_id:new ObjectID(result["comprador"])});
+		comprador = response["name"];
+		for (let x = 0; x < lista.length; x+=2) {
+			prods = {};
+			response = await prod.findOne({_id:new ObjectID(lista[x])});
+			prods["name"] = response["name"];
+			prods["pre"] = response["pre"];
+			prods["cant"] = lista[x+1];
+			carrito[cont] = prods;
+			cont++;
+		}
+		orden[comprador] = carrito;
+		res.send(orden);
+	});
 });
 
 app.listen(3000, function() {
